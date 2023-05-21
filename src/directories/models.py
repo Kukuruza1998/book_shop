8f30f1333b0a5fc0bd4fa6d5307e3976baf76d1c
Kukuruza Alexandr <mrsilly998@mail.ru>
2023-05-21 22:58:33 +0300

    create models

--- src/directories/models.py
+++ src/directories/models.py
@@ -1,3 +1,97 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    autor_name = models.CharField(
+        verbose_name = 'Autor name',
+        max_length = 50
+    )
+    autor_description = models.TextField(
+        verbose_name = 'Autor description',
+        null = True,
+        blank = True
+    )
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Series(models.Model):
+    series_name = models.CharField(
+        verbose_name = 'Series',
+        max_length = 100
+    )
+    series_description = models.TextField(
+        verbose_name = "Count book's series",
+        null = True,
+        blank = True
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Genre(models.Model):
+    genre_name = models.CharField(
+        verbose_name = 'Genre',
+        max_length = 50
+    )
+    genre_description = models.TextField(
+        verbose_name = 'Genre description',
+        null = True,
+        blank = True
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Publishing_House(models.Model):
+    publishing_house_name = models.CharField(
+        verbose_name = 'Publishing house name',
+        max_length = 50
+    )
+    publishing_house_description = models.TextField(
+        verbose_name = 'Publishing house description',
+        null = True,
+        blank = True
+    )
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Book(models.Model):
+    book_name = models.CharField(
+        verbose_name = 'Book name',
+        max_length = 50
+    )
+    book_image = models.ImageField(
+        upload_to = 'book',
+        blank = True
+    )
+    book_price = models.DecimalField(
+        max_digits = 6,
+        decimal_places = 2,
+        default = 0
+    )
+    autor = models.ForeignKey(
+        Autor,
+        on_delete = models.PROTECT,
+        verbose_name = 'autor'
+    )
+    series = models.ForeignKey(
+        Series,
+        on_delete = models.PROTECT,
+        verbose_name = 'series'
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete = models.PROTECT,
+        verbose_name = 'genre'
+    )
+    year_publishing = models.DateField(
+        verbose_name = 'Year publishing book',
+        max_length = 50
+    )
