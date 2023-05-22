5ceaef8a1b607a15e443198aa4f80888662fa8f0
Kukuruza Alexandr <mrsilly998@mail.ru>
2023-05-22 13:50:02 +0300

    add more models

--- src/directories/models.py
+++ src/directories/models.py
@@ -95,6 +95,80 @@ class Book(models.Model):
         verbose_name = 'Year publishing book',
         max_length = 50
     )
-    
+    page = models.DecimalField(
+        max_digits = 6,
+        decimal_places = 0,
+        default = 0
+    )
+    BINDING = (
+        ('Solid', 'solid'),
+        ('Soft', 'soft'),
+        ('Absent', 'absent'),
+    )
+
+    binding = models.CharField(
+        verbose_name = 'Binding book',
+        max_length = 6,
+        choices = BINDING
+    )
+        
+    FORMAT = (
+        ('Super small', '84x108/64'),
+        ('Small', '75x90/32'),
+        ('Standart', '60x90/16'),
+        ('Bigger', '70x108/16'),
+        ('Big', '60x90/8'),
+        ('Super big', '84x108/8'),
+    )
+
+    format_book = models.CharField(
+        verbose_name = 'Book format',
+        max_length = 12,
+        choices = FORMAT
+    )
+    ISBN = models.CharField(
+        verbose_name = 'ISBN',
+        max_length = 25
+    )
+
+    weight = models.DecimalField(
+        verbose_name = 'Book weight(gramme)',
+        max_digits = 6,
+        decimal_places = 1,
+        default = 0
+    )
+    age_restrictions = models.DecimalField(
+        verbose_name = 'Age restrictions',
+        max_digits = 3,
+        decimal_places = 0,
+        default = 0
+    )
+    publishing_house = models.ForeignKey(
+        Publishing_House,
+        on_delete = models.PROTECT,
+        verbose_name = 'Publishing house'
+    )
+    counter_book = models.DecimalField(
+        verbose_name = 'Count of books available',
+        max_digits = 7,
+        decimal_places = 0,
+        default = 0
+    )
+    ACTIVE = (
+        ('Y', 'active'),
+        ('N', 'inactive'),
+    )
+
+    active = models.CharField(
+        verbose_name = 'Book active',
+        max_length = 4,
+        choices = ACTIVE
+    )
+    rating = models.DecimalField(
+        verbose_name = 'Rating',
+        max_digits = 2,
+        decimal_places = 1,
+        default = 0
+    )
     def __str__(self) -> str:
         return self.book_name
